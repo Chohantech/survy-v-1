1d06b793be4d028aca9e43ac578e70bb2b6ceebc
Chohantech <jm2815428@gmail.com>
2025-12-12 11:22:57 +0500

    update

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -3,6 +3,35 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { SignJWT } from "jose";
 import clientPromise from "./db";
 import { sendResetEmail } from "./actions/email";
+const domain = process.env.NEXT_PUBLIC_DOMAIN;
+const isProd = process.env.NODE_ENV === "production";
+
+const cookies = {
+  sessionToken: {
+    name: "better-auth.session",
+    path: "/",
+    secure: isProd,
+    httpOnly: true,
+    sameSite: "none",
+    domain: isProd ? domain : undefined,
+  },
+  csrfToken: {
+    name: "better-auth.csrf",
+    path: "/",
+    secure: isProd,
+    httpOnly: false,
+    sameSite: "none",
+    domain: isProd ? domain : undefined,
+  },
+  callbackUrl: {
+    name: "better-auth.callback-url",
+    path: "/",
+    secure: isProd,
+    httpOnly: false,
+    sameSite: "none",
+    domain: isProd ? domain : undefined,
+  },
+};
 
 const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET);
 
@@ -17,33 +46,9 @@ async function createCustomToken(sessionId: string, userId: string) {
 const client = await clientPromise;
 
 export const auth = betterAuth({
-  database: mongodbAdapter(client.db("social-network")),
-
-cookies: {
-  sessionToken: {
-    name: "better-auth.session",
-    path: "/",
-    secure: true,
-    httpOnly: true,
-    sameSite: "none",
-  },
-  csrfToken: {
-    name: "better-auth.csrf",
-    path: "/",
-    secure: true,
-    httpOnly: false,
-    sameSite: "none",
-  },
-  callbackUrl: {
-    name: "better-auth.callback-url",
-    path: "/",
-    secure: true,
-    httpOnly: false,
-    sameSite: "none",
-  },
-},
-
-
+  database: mongodbAdapter(client.db("svyrndb")),
+cookies
+,
   emailAndPassword: {
     enabled: true,
     requireEmailVerification: false,
--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import { config } from "./config";
 
 const initSocket = ({ token }: { token: string }) => {
-  const socket = io(config.apiUrl || "http://localhost:4000", {
+  const socket = io(config.apiUrl || "http://localhost:5000", {
     auth: {
       token: typeof window !== "undefined" ? token : undefined,
     },
