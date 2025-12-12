02c3f1f5d6571ecac8748370627915d3bada01f5
Chohantech <jm2815428@gmail.com>
2025-12-12 09:38:30 +0500

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  console.log("MONGODB_URI: ", process.env.MONGODB_URI)
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      dbName: 'social-network'
-    });
+    console.log("Connecting to MongoDB...");
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error('Database connection error:', error);
+    console.error("Database connection error:", error);
     process.exit(1);
   }
 };
